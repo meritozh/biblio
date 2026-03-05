820cc41a9ac138ee8c801bd53d03504f8746d483
meritozh <ah841814092@gmail.com>
2026-03-05 10:48:36 +0800

    chore: track router genereted file, so make ci works

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
+import { tanstackRouter } from '@tanstack/router-vite-plugin';
 import path from 'path';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), TanStackRouterVite()],
+  plugins: [react(), tailwindcss(), tanstackRouter()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
